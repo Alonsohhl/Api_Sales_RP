96919ab51af9d8429dafb9ac16ecb94fdd8dd1d6
alonsohhl <alonso.hl25@gmail.com>
2019-05-30 21:21:06 -0700

    fixing CRUD

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,26 +1,40 @@
-const userController = {};
+const userController = {}
 
-var pool=require("./db/connection")
+var pool = require('./db/connection')
 
-userController.insert = async (req,res) =>{
-
-    const {Dni_Cli,Nom_Cli,App_Cli,Apm_Cli,Correo_Cli,Sexo_Cli} = req.body;
-    const newUser = {
-        Dni_Cli,
-        Nom_Cli,
-        App_Cli,
-        Apm_Cli,
-        Correo_Cli,
-        Sexo_Cli
-    }
-    await pool.query("insert into t01fcli  set  ?",[newUser])
-    .catch( err => {
-        res.send('Error:  '+err.sqlMessage);
-        throw(error);
-    });
-    console.log("User Insert");
-    // console.dir(pool)
-    res.send('received');
+userController.insert = async (req, res) => {
+  // eslint-disable-next-line camelcase
+  const { Dni_Cli, Nom_Cli, App_Cli, Apm_Cli, Correo_Cli, Sexo_Cli } = req.body
+  const newUser = {
+    Dni_Cli,
+    Nom_Cli,
+    App_Cli,
+    Apm_Cli,
+    Correo_Cli,
+    Sexo_Cli
+  }
+  await pool.query('insert into t01fcli  set  ?', [newUser])
+    .catch(err => {
+      res.send('Error:  ' + err.sqlMessage)
+      throw (err)
+    })
+  console.log('User Insert')
+  // console.dir(pool)
+  res.send('User Insert')
 }
 
-module.exports = userController;
+userController.udpUser = async (req, res) => {
+  // eslint-disable-next-line camelcase
+  const { Dni_Cli, App_Cli } = req.body
+  // eslint-disable-next-line camelcase
+  await pool.query('UPDATE    t01fcli    SET    App_Cli= ?    WHERE    Dni_Cli = ?', [App_Cli, Dni_Cli])
+    .catch(err => {
+      res.send('Error:  ' + err.sqlMessage)
+      throw (err)
+    })
+
+  //    TODO usar linter
+  res.send('Updated')
+}
+
+module.exports = userController
--- src/app/routes/appRoutes.js
+++ src/app/routes/appRoutes.js
@@ -1,26 +1,24 @@
 const router = require('express').Router();
 
-const appController = require('../controllers/appController');
-const userController = require("../controllers/userController");
+const appController = require('../controllers/appController')
+const userController = require('../controllers/userController')
 
-const appCustomValidator = require("./routeValidator").appCustomValidator;
-
+// const appCustomValidator = require("./routeValidator").appCustomValidator;
 
 // router.get('/', appController.index);
 
-/*====================== Empresa ====================*/
-router.post('/insertarEmp', appController.insEmp); //ingresa empresa
+/* ====================== Empresa ==================== */
+router.post('/insertarEmp', appController.insEmp) // Create User
 // router.post('/insProp', appController.insProp); //Ingresa propuesta de empresa
-
-
 // /*====================== Usuario ====================*/
-router.post("/insUsu",userController.insert)
+router.post('/insUsu', userController.insert)
+router.post('/udpUser', userController.udpUser) //     Update User
 // router.post('/insUsu', appCustomValidator.VALIDATE_REGISTER,   appController.insUsu); //ingresa usuario
 // router.post('/insUsuProp/', appController.insUsuProp); //ingresa propuesta usuario
 // router.post('/usuLogin/', appController.usuLogin); //ingresa propuesta usuario
 // router.post('/login/', appController.login); //ingresa propuesta usuario
 
 // router.get('/getPropUsu', appController.GetPropUsu);
-//https://carlosazaustre.es/como-relacionar-tus-modelos-en-mongodb/
+// https://carlosazaustre.es/como-relacionar-tus-modelos-en-mongodb/
 
-module.exports = router;
+module.exports = router
